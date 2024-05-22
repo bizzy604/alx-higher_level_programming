c317c8a4fed519ba8aee857bad1c01ffeb0ea1e6
bizzy604 <kevinyamatta50@gmail.com>
2024-05-22 11:13:32 -0700

    Automated commit

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,4 +1,4 @@
 -- creating a user_0d_1, should have all privileges, password set to user_0d_1_pwd
 -- if user already exists, script shoul not fail
-CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED WITH 'user_0d_1_pwd';
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
 GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
