8763797cc8a52c7e9bf1ec34513d110e3f9df14a
bizzy604 <kevinyamatta50@gmail.com>
2024-05-22 11:40:25 -0700

    Automated commit

---
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -0,0 +1,6 @@
+-- create a database hbtn_0d_usa, table states on MySQL server
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+USE hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS states(
+	id INT UNIQUE AUTO_INCREAMENT PRIMARY KEY,
+	name VARCHAR(256) NOT NULL);
